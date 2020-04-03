e6431e23a4591010a00870ce5cb613ee6855d478
rivanildojr <rivanildo_junior7@hotmail.com>
2020-04-03 02:30:10 -0300

    configurando o sequelize e criado a  migration de users, add a port do mysql no docker

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.PORT_DATABASE,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
---
+++ backend/src/database/migrations/20200403044910-create-users.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      login: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  },
+};
